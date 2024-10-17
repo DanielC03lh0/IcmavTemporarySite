e0dd6f9bb2f601a844c008076b7fa88fb3015ef5
Daniel Coelho <daniel.m.coelho@outlook.pt>
2024-10-17 16:25:27 +0100

    Convert tailwind to default css
    + Convert tailwind to default css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '**/*.html', // Procura classes Tailwind em todos os arquivos HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
